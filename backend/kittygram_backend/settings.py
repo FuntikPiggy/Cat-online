dea51475dfd04928e8674ffa18c8c9179287879c
Gurin Valeriy <gurin.valerii@gmail.com>
2025-07-30 16:38:28 +0600

    main 05commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -114,5 +114,8 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-
 }
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
